3e5caf6085f8b6c063b5dbccd444fdbee9fdbca9
Diego Barneche <barnechedr@gmail.com>
2023-02-27 22:10:12 +0800

    updated some details; added code for figure 3

--- figs.R
+++ figs.R
@@ -1,8 +1,43 @@
 library(tidyverse)
 library(patchwork)
 library(brms)
-options(cores = parallel::detectCores())
+library(ggdist)
+options(mc.cores = parallel::detectCores())
 dir.create("output", recursive = TRUE, showWarnings = FALSE)
+
+annotate_text <- function(label, x, y, facets = NULL, hjust = 0,
+                          vjust = 0, color = "black", alpha = NA,
+                          family = thm$text$family,
+                          size = thm$text$size, fontface = 1, line_height = 1.0,
+                          box_just = ifelse(c(x, y) < 0.5, 0, 1),
+                          margin = grid::unit(size / 2, "pt"),
+                          thm = theme_get()) {
+# from stackoverflow
+# question 22488563
+# ggplot2-annotate-layer-position-in-r
+  x <- scales::squish_infinite(x)
+  y <- scales::squish_infinite(y)
+
+  tg <- grid::textGrob(label, x = 0, y = 0, hjust = hjust, vjust = vjust,
+                       gp = grid::gpar(col = alpha(color, alpha),
+                                       fontsize = size, fontfamily = family,
+                                       fontface = fontface,
+                                       lineheight = line_height))
+  ts <- grid::unit.c(grid::grobWidth(tg), grid::grobHeight(tg))
+  vp <- grid::viewport(x = x, y = y, width = ts[1], height = ts[2],
+                       just = box_just)
+  tg <- grid::editGrob(tg, x = ts[1] * hjust, y = ts[2] * vjust, vp = vp)
+  unt <- grid::unit(1, "npc") - margin * 2
+  inr <- grid::grobTree(tg, vp = grid::viewport(width = unt, height = unt))
+
+  layer(data = NULL, stat = StatIdentity, position = PositionIdentity,
+        geom = GeomCustomAnn, inherit.aes = TRUE,
+        params = list(grob = grid::grobTree(inr),
+                      xmin = -Inf,
+                      xmax = Inf,
+                      ymin = -Inf,
+                      ymax = Inf))
+}
 
 rounded <- function(value, precision = 1) {
   sprintf(paste0("%.", precision, "f"), round(value, precision))
@@ -116,13 +151,13 @@ M <- exp(rnorm(1e3, log(500)))
 set.seed(10)
 B_hetero_error <- rnorm(length(M), log(met_rates_m(b_o, alpha, M)), 0.2)
 fig_2a_df <- data.frame(x = M, y = exp(B_hetero_error))
-priors <- prior(normal(0, 1), nlpar = "botc") +
+fig_2a_priors <- prior(normal(0, 1), nlpar = "botc") +
   prior(normal(0, 1), nlpar = "alpha")
 fig_2a_mod <- brms::brm(
   bf(y ~ botc * x^alpha, botc + alpha ~ 1, nl = TRUE),
-  data = fig_2a_df, family = Gamma(link = "identity"), prior = priors,
+  data = fig_2a_df, family = Gamma(link = "identity"), prior = fig_2a_priors,
   chains = 4, iter = 2000, warmup = 1000, thin = 1, seed = 10,
-  cores = 4
+  cores = 4, save_pars = TRUE
 )
 cols <- c("Estimate", "Q2.5", "Q97.5")
 fig_2a_botc <- rounded(brms::fixef(fig_2a_mod)["botc_Intercept", cols], 2)
@@ -132,11 +167,11 @@ fake_x <- data.frame(
 )
 preds_g <- cbind(fake_x, data.frame(predict(fig_2a_mod, newdata = fake_x)))
 fig_2b_df <- log(fig_2a_df)
-priors <- prior(normal(0, 1), class = "b")
+fig_2b_priors <- prior(normal(0, 1), class = "b")
 fig_2b_mod <- brms::brm(
   y ~ 0 + Intercept + x, data = fig_2b_df, family = gaussian(),
-  prior = priors, chains = 4, iter = 2000, warmup = 1000, thin = 1, seed = 10,
-  cores = 4
+  prior = fig_2b_priors, chains = 4, iter = 2000, warmup = 1000, thin = 1,
+  seed = 10, cores = 4, save_all_pars = TRUE
 )
 cols <- c("Estimate", "Q2.5", "Q97.5")
 fig_2b_botc <- rounded(brms::fixef(fig_2b_mod)["Intercept", cols], 2)
@@ -241,3 +276,156 @@ fig_2b <- ggplot() +
   )
 
 ggsave("output/figure_2.pdf", fig_2a + fig_2b, width = 7.13, height = 3.23)
+
+fig_3_null_priors <- prior(normal(0, 1), class = "b", coef = "Intercept") +
+  prior(normal(0.67, 0.01), class = "b", coef = "x")
+fig_3_null_mod <- brms::brm(
+  y ~ 0 + Intercept + x, data = fig_2b_df, family = gaussian(),
+  prior = fig_3_null_priors, chains = 4, iter = 2e4, warmup = 1e4, thin = 1,
+  seed = 10, cores = 4, save_pars = save_pars(all = TRUE),
+  sample_prior = "yes"
+)
+
+fig_3_alt_priors <- prior(normal(0, 1), class = "b", coef = "Intercept") +
+  prior(normal(0.75, 0.01), class = "b", coef = "x")
+fig_3_alt_mod <- brms::brm(
+  y ~ 0 + Intercept + x, data = fig_2b_df, family = gaussian(),
+  prior = fig_3_alt_priors, chains = 4, iter = 2e4, warmup = 1e4, thin = 1,
+  seed = 10, cores = 4, save_pars = save_pars(all = TRUE),
+  sample_prior = "yes"
+)
+
+options(mc.cores = 1)
+set.seed(10)
+bf_mods_fig_3 <- brms::bayes_factor(fig_3_alt_mod, fig_3_null_mod)
+set.seed(10)
+pp_mods_fig_3 <- brms::post_prob(fig_3_alt_mod, fig_3_null_mod, prior_prob = c(0.5, 0.5))
+
+# So in the case of equal prior probabilities, BF == Posterior odds
+all.equal(bf_mods_fig_3$bf, pp_mods_fig_3[[1]] / pp_mods_fig_3[[2]])
+
+prior_df <- cbind(
+  brms::as_draws_df(fig_3_null_mod) %>%
+    dplyr::select(M1 = prior_b_x),
+  brms::as_draws_df(fig_3_alt_mod) %>%
+    dplyr::select(M2 = prior_b_x)
+) %>%
+  dplyr::mutate(Draw = seq_len(n())) %>%
+  tidyr::pivot_longer(
+    cols = c("M1", "M2"), names_to = "Model"
+  )
+
+post_df <- cbind(
+  brms::as_draws_df(fig_3_null_mod) %>%
+    dplyr::select(M1 = b_x),
+  brms::as_draws_df(fig_3_alt_mod) %>%
+    dplyr::select(M2 = b_x)
+) %>%
+  dplyr::mutate(Draw = seq_len(n())) %>%
+  tidyr::pivot_longer(
+    cols = c("M1", "M2"), names_to = "Model"
+  )
+
+dens_pr <- ggplot(data = prior_df) +
+  stat_halfeye(mapping = aes(x = value, fill = Model), adjust = 2) +
+  scale_x_continuous(breaks = c(0.67, 0.75), limits = c(0.63, 0.79)) +
+  labs(y = "Density", x = substitute(italic(alpha)), fill = "Model: ") +
+  annotate_text(
+    x = 0, y = 1.05,
+    label = substitute("Prior distributions of " * italic(alpha))
+  ) +
+  scale_fill_manual(
+    values = c("tomato", "dodgerblue3"),
+    labels = c(
+      expression(paste(italic("M"[1]))), expression(paste(italic("M"[2])))
+    )
+  ) +
+  theme_classic() +
+  theme(
+    panel.background = element_blank(),
+    plot.margin = margin(0, 0, 0, 0, "cm"),
+    plot.background = element_blank()
+  )
+
+dens_po <- ggplot(data = post_df) +
+  stat_halfeye(mapping = aes(x = value, fill = Model), adjust = 2) +
+  scale_x_continuous(breaks = c(0.67, 0.75), limits = c(0.63, 0.79)) +
+  labs(y = "Density", x = substitute(italic(alpha)), fill = "Model: ") +
+  annotate_text(
+    x = 0, y = 1.05,
+    label = substitute("Posterior distributions of " * italic(alpha))
+  ) +
+  scale_fill_manual(
+    values = c("tomato", "dodgerblue3"),
+    labels = c(
+      expression(paste(italic("M"[1]))), expression(paste(italic("M"[2])))
+    )
+  ) +
+  theme_classic() +
+  theme(
+    panel.background = element_blank(),
+    plot.margin = margin(0, 0, 0, 0, "cm"),
+    plot.background = element_blank()
+  )
+
+prob_pr <- ggplot(data = data.frame(y = c(0.5, 0.5), x = 1:2)) +
+  geom_point(mapping = aes(x, y), shape = c(15, 16), size = 3,
+             colour = c("tomato", "dodgerblue3")) +
+  annotate_text(x = 0, y = 1.05, label = "Prior model probs.") +
+  scale_y_continuous(breaks = c(0, 0.5, 1), limits = c(-0.05, 1.3)) +
+  scale_x_continuous(
+    breaks = c(1, 2), limits = c(-0, 3),
+    labels = c(
+      expression(paste(italic("M"[1]))), expression(paste(italic("M"[2])))
+    )
+  ) +
+  labs(x = "", y = "") +
+  theme_classic() +
+  theme(
+    axis.title = element_text(size = .pt * 4, colour = "black"),
+    axis.text = element_text(size = .pt * 3, colour = "black"),
+    panel.background = element_blank(),
+    plot.margin = margin(0, 0, 0, 0, "cm"),
+    plot.background = element_blank()
+  ) +
+  coord_cartesian(clip = "off")
+
+prob_po <- ggplot(data = data.frame(y = rev(pp_mods_fig_3), x = 1:2)) +
+  geom_point(mapping = aes(x, y), shape = c(15, 16), size = 3,
+             colour = c("tomato", "dodgerblue3")) +
+  annotate_text(x = 0, y = 1.05, label = "Post. model probs.") +
+  scale_y_continuous(breaks = c(0, 0.5, 1), limits = c(-0.05, 1.3)) +
+  scale_x_continuous(
+    breaks = c(1, 2), limits = c(-0, 3),
+    labels = c(
+      expression(paste(italic("M"[1]))), expression(paste(italic("M"[2])))
+    )
+  ) +
+  labs(x = "", y = "") +
+  theme_classic() +
+  theme(
+    axis.title = element_text(size = .pt * 4, colour = "black"),
+    axis.text = element_text(size = .pt * 3, colour = "black"),
+    panel.background = element_blank(),
+    plot.margin = margin(0, 0, 0, 0, "cm"),
+    plot.background = element_blank()
+  ) +
+  coord_cartesian(clip = "off")
+
+layout <- "
+#AAA#####BBB###
+#AAA#####BBB###
+#AAA#####BBB###
+CCCCCCC#DDDDDDD
+CCCCCCC#DDDDDDD
+CCCCCCC#DDDDDDD
+CCCCCCC#DDDDDDD
+CCCCCCC#DDDDDDD
+###############
+"
+
+fig_3 <- prob_pr + prob_po + dens_pr + dens_po +
+  plot_layout(guides = "collect", design = layout) &
+  theme(legend.position = "top")
+
+ggsave("output/figure_3.pdf", fig_3, width = 7, height = 3.8)
