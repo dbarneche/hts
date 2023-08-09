1ab53a99a2e5f84af2200be3ed6b390e3524a2bc
Diego Barneche <barnechedr@gmail.com>
2023-08-09 20:43:04 +0800

    revised version

--- figs.R
+++ figs.R
@@ -146,182 +146,75 @@ fig_1b <- ggplot(data = data.frame(x = T, y = B_2)) +
 ggsave("output/figure_1.pdf", fig_1a + fig_1b, width = 7.13, height = 3.23)
 
 # Figure 2
-set.seed(2)
-M <- exp(rnorm(1e3, log(500)))
-set.seed(10)
-B_hetero_error <- rnorm(length(M), log(met_rates_m(b_o, alpha, M)), 0.2)
-fig_2a_df <- data.frame(x = M, y = exp(B_hetero_error))
-fig_2a_priors <- prior(normal(0, 1), nlpar = "botc") +
-  prior(normal(0, 1), nlpar = "alpha")
-fig_2a_mod <- brms::brm(
-  bf(y ~ botc * x^alpha, botc + alpha ~ 1, nl = TRUE),
-  data = fig_2a_df, family = Gamma(link = "identity"), prior = fig_2a_priors,
-  chains = 4, iter = 2000, warmup = 1000, thin = 1, seed = 10,
-  cores = 4, save_pars = TRUE
-)
-cols <- c("Estimate", "Q2.5", "Q97.5")
-fig_2a_botc <- rounded(brms::fixef(fig_2a_mod)["botc_Intercept", cols], 2)
-fig_2a_alpha <- rounded(brms::fixef(fig_2a_mod)["alpha_Intercept", cols], 2)
-fake_x <- data.frame(
-  x = seq(min(fig_2a_df$x), max(fig_2a_df$x), length.out = 100)
-)
-preds_g <- cbind(fake_x, data.frame(predict(fig_2a_mod, newdata = fake_x)))
-fig_2b_df <- log(fig_2a_df)
-fig_2b_priors <- prior(normal(0, 1), class = "b")
-fig_2b_mod <- brms::brm(
-  y ~ 0 + Intercept + x, data = fig_2b_df, family = gaussian(),
-  prior = fig_2b_priors, chains = 4, iter = 2000, warmup = 1000, thin = 1,
-  seed = 10, cores = 4, save_all_pars = TRUE
-)
-cols <- c("Estimate", "Q2.5", "Q97.5")
-fig_2b_botc <- rounded(brms::fixef(fig_2b_mod)["Intercept", cols], 2)
-fig_2b_alpha <- rounded(brms::fixef(fig_2b_mod)["x", cols], 2)
-fake_x <- data.frame(
-  x = seq(min(fig_2b_df$x), max(fig_2b_df$x), length.out = 100)
-)
-preds_n <- cbind(fake_x, data.frame(predict(fig_2b_mod, newdata = fake_x)))
-fig_2a <- ggplot() +
-  geom_ribbon(
-    data = preds_g, mapping = aes(x = x, ymin = `Q2.5`, ymax = `Q97.5`),
-    fill = "grey90", alpha = 0.8
-  ) +
-  geom_point(data = fig_2a_df, mapping = aes(x, y), alpha = 0.5, size = 2) +
-  geom_line(
-    data = preds_g, mapping = aes(x = x, y = Estimate),
-    colour = "tomato", linetype = 2
-  ) +
-  labs(x = substitute("Body mass, " * italic("M")),
-       y = substitute("Metabolic rate, " * italic("B")),
-       subtitle = substitute(
-         italic("b"["o"]) * "(" * italic("T"["c"]) * ") = 5; " * alpha *
-           " = 0.75"
-       )
-  ) +
-  annotate(
-    geom = "text", x = 0, y = 7500, hjust = 0, vjust = 0.5, size = 3,
-    label = paste0(deparse(
-      substitute(
-        italic("B") == a %.% italic("M")^b,
-        list(a = fig_2a_botc[1], b = fig_2a_alpha[1])
-      )
-    ), collapse = ""), parse = TRUE
-  ) +
-  annotate(
-    geom = "text", x = 0, y = 6800, hjust = 0, vjust = 0.5, size = 3,
-    label = paste0(deparse(
-      substitute(
-        "95% C.I. " * italic("b"["o"]) * "(" * italic("T"["c"]) * "): " *
-          a - b, list(a = fig_2a_botc[2], b = fig_2a_botc[3])
-      )
-    ), collapse = ""), parse = TRUE
-  ) +
-  annotate(
-    geom = "text", x = 0, y = 6100, hjust = 0, vjust = 0.5, size = 3,
-    label = paste0(deparse(
-      substitute(
-        "95% C.I. " * alpha * ": " * a - b,
-        list(a = fig_2a_alpha[2], b = fig_2a_alpha[3])
-      )
-    ), collapse = ""), parse = TRUE
-  ) +
-  theme_classic() +
-  theme(
-     axis.title = element_text(size = .pt * 4, colour = "black"),
-     axis.text = element_text(size = .pt * 3, colour = "black")
-  )
+data <- read.csv("respiration_data.csv") |>
+  dplyr::rename_with(tolower)
 
-fig_2b <- ggplot() +
-  geom_ribbon(
-    data = preds_n, mapping = aes(x = x, ymin = `Q2.5`, ymax = `Q97.5`),
-    fill = "grey90", alpha = 0.8
-  ) +
-  geom_point(data = fig_2b_df, mapping = aes(x, y), alpha = 0.5, size = 2) +
-  geom_line(
-    data = preds_n, mapping = aes(x = x, y = Estimate),
-    colour = "tomato", linetype = 2
-  ) +
-  labs(x = substitute("ln Body mass, ln" * italic("M")),
-       y = substitute("ln Metabolic rate, ln" * italic("B"))) +
-  annotate(
-    geom = "text", x = 3, y = 8.6, hjust = 0, vjust = 0.5, size = 3,
-    label = paste0(deparse(
-      substitute(
-        "ln" * italic("B") == a + b %.% "ln" * italic("M"),
-        list(a = fig_2b_botc[1], b = fig_2b_alpha[1])
-      )
-    ), collapse = ""), parse = TRUE
-  ) +
-  annotate(
-    geom = "text", x = 3, y = 8.1, hjust = 0, vjust = 0.5, size = 3,
-    label = paste0(deparse(
-      substitute(
-        "95% C.I. " * "ln" * italic("b"["o"]) * "(" * italic("T"["c"]) *
-          "): " * a - b, list(a = fig_2b_botc[2], b = fig_2b_botc[3])
-      )
-    ), collapse = ""), parse = TRUE
-  ) +
-  annotate(
-    geom = "text", x = 3, y = 7.6, hjust = 0, vjust = 0.5, size = 3,
-    label = paste0(deparse(
-      substitute(
-        "95% C.I. " * alpha * ": " * a - b,
-        list(a = fig_2b_alpha[2], b = fig_2b_alpha[3])
-      )
-    ), collapse = ""), parse = TRUE
-  ) +
-  theme_classic() +
-  theme(
-     axis.title = element_text(size = .pt * 4, colour = "black"),
-     axis.text = element_text(size = .pt * 3, colour = "black")
-  )
-
-ggsave("output/figure_2.pdf", fig_2a + fig_2b, width = 7.13, height = 3.23)
-
-fig_3_null_priors <- prior(normal(0, 1), class = "b", coef = "Intercept") +
-  prior(normal(0.67, 0.01), class = "b", coef = "x")
-fig_3_null_mod <- brms::brm(
-  y ~ 0 + Intercept + x, data = fig_2b_df, family = gaussian(),
-  prior = fig_3_null_priors, chains = 4, iter = 2e4, warmup = 1e4, thin = 1,
-  seed = 10, cores = 4, save_pars = save_pars(all = TRUE),
-  sample_prior = "yes"
+boltz_inits <- list(
+  list(lnMass = 0.4, invKT = 0.8),
+  list(lnMass = 0.8, invKT = 0.75),
+  list(lnMass = 1.0, invKT = 0.6)
 )
 
-fig_3_alt_priors <- prior(normal(0, 1), class = "b", coef = "Intercept") +
-  prior(normal(0.75, 0.01), class = "b", coef = "x")
-fig_3_alt_mod <- brms::brm(
-  y ~ 0 + Intercept + x, data = fig_2b_df, family = gaussian(),
-  prior = fig_3_alt_priors, chains = 4, iter = 2e4, warmup = 1e4, thin = 1,
-  seed = 10, cores = 4, save_pars = save_pars(all = TRUE),
-  sample_prior = "yes"
+priors_twothirds <- prior(normal(0.67, 0.01), class = "b", coef = "lnmass_g") +
+  prior(normal(1, 2), class = "b", coef = "invkt") +
+  prior(normal(3, 3), class = "Intercept") +
+  prior(student_t(3, 0, 20), "sd") +
+  prior(student_t(3, 0, 20), "sigma")
+
+model_twothirds <- brms::brm(
+  lnresprate_j_d ~ lnmass_g + invkt + (1 + lnmass_g | fishid), data = data,
+  family = gaussian(), prior = priors_twothirds, sample_prior = TRUE,
+  chains = 3, cores = 3, iter = 3e4, warmup = 1.5e4, init = boltz_inits,
+  control = list(adapt_delta = 0.99, max_treedepth = 15), seed = 10,
+  save_pars = save_pars(all = TRUE)
+)
+
+priors_threequart <- prior(normal(0.75, 0.01), class = "b", coef = "lnmass_g") +
+  prior(normal(1, 2), class = "b", coef = "invkt") +
+  prior(normal(3, 3), class = "Intercept") +
+  prior(student_t(3, 0, 20), "sd") +
+  prior(student_t(3, 0, 20), "sigma")
+
+model_threequart <- brms::brm(
+  lnresprate_j_d ~ lnmass_g + invkt + (1 + lnmass_g | fishid), data = data,
+  family = gaussian(), prior = priors_threequart, sample_prior = TRUE,
+  chains = 3, cores = 3, iter = 3e4, warmup = 1.5e4, init = boltz_inits,
+  control = list(adapt_delta = 0.99, max_treedepth = 15), seed = 10,
+  save_pars = save_pars(all = TRUE)
 )
 
 options(mc.cores = 1)
 set.seed(10)
-bf_mods_fig_3 <- brms::bayes_factor(fig_3_alt_mod, fig_3_null_mod)
+bf_mods_fig_2 <- brms::bayes_factor(
+  model_threequart, model_twothirds
+)
 set.seed(10)
-pp_mods_fig_3 <- brms::post_prob(fig_3_alt_mod, fig_3_null_mod, prior_prob = c(0.5, 0.5))
+pp_mods_fig_2 <- brms::post_prob(
+  model_threequart, model_twothirds, prior_prob = c(0.5, 0.5)
+)
 
 # So in the case of equal prior probabilities, BF == Posterior odds
-all.equal(bf_mods_fig_3$bf, pp_mods_fig_3[[1]] / pp_mods_fig_3[[2]])
+all.equal(bf_mods_fig_2$bf, pp_mods_fig_2[[1]] / pp_mods_fig_2[[2]])
+pp_mods_fig_2[[2]] / pp_mods_fig_2[[1]]
 
 prior_df <- cbind(
-  brms::as_draws_df(fig_3_null_mod) %>%
-    dplyr::select(M1 = prior_b_x),
-  brms::as_draws_df(fig_3_alt_mod) %>%
-    dplyr::select(M2 = prior_b_x)
-) %>%
-  dplyr::mutate(Draw = seq_len(n())) %>%
+  brms::as_draws_df(model_twothirds) |>
+    dplyr::select(M1 = prior_b_lnmass_g),
+  brms::as_draws_df(model_threequart) |>
+    dplyr::select(M2 = prior_b_lnmass_g)
+) |>
+  dplyr::mutate(Draw = seq_len(n())) |>
   tidyr::pivot_longer(
     cols = c("M1", "M2"), names_to = "Model"
   )
 
 post_df <- cbind(
-  brms::as_draws_df(fig_3_null_mod) %>%
-    dplyr::select(M1 = b_x),
-  brms::as_draws_df(fig_3_alt_mod) %>%
-    dplyr::select(M2 = b_x)
-) %>%
-  dplyr::mutate(Draw = seq_len(n())) %>%
+  brms::as_draws_df(model_twothirds) |>
+    dplyr::select(M1 = b_lnmass_g),
+  brms::as_draws_df(model_threequart) |>
+    dplyr::select(M2 = b_lnmass_g)
+) |>
+  dplyr::mutate(Draw = seq_len(n())) |>
   tidyr::pivot_longer(
     cols = c("M1", "M2"), names_to = "Model"
   )
@@ -390,7 +283,7 @@ prob_pr <- ggplot(data = data.frame(y = c(0.5, 0.5), x = 1:2)) +
   ) +
   coord_cartesian(clip = "off")
 
-prob_po <- ggplot(data = data.frame(y = rev(pp_mods_fig_3), x = 1:2)) +
+prob_po <- ggplot(data = data.frame(y = rev(pp_mods_fig_2), x = 1:2)) +
   geom_point(mapping = aes(x, y), shape = c(15, 16), size = 3,
              colour = c("tomato", "dodgerblue3")) +
   annotate_text(x = 0, y = 1.05, label = "Post. model probs.") +
@@ -424,8 +317,139 @@ CCCCCCC#DDDDDDD
 ###############
 "
 
-fig_3 <- prob_pr + prob_po + dens_pr + dens_po +
+fig_2 <- prob_pr + prob_po + dens_pr + dens_po +
   plot_layout(guides = "collect", design = layout) &
   theme(legend.position = "top")
+ggsave("output/figure_2.pdf", fig_2, width = 7, height = 3.8)
 
-ggsave("output/figure_3.pdf", fig_3, width = 7, height = 3.8)
+# Figure 3
+set.seed(2)
+M <- exp(rnorm(1e3, log(500)))
+set.seed(10)
+B_hetero_error <- rnorm(length(M), log(met_rates_m(b_o, alpha, M)), 0.2)
+fig_3a_df <- data.frame(x = M, y = exp(B_hetero_error))
+fig_3a_priors <- prior(normal(0, 1), nlpar = "botc") +
+  prior(normal(0, 1), nlpar = "alpha")
+fig_3a_mod <- brms::brm(
+  bf(y ~ botc * x^alpha, botc + alpha ~ 1, nl = TRUE),
+  data = fig_3a_df, family = Gamma(link = "identity"), prior = fig_3a_priors,
+  chains = 4, iter = 2000, warmup = 1000, thin = 1, seed = 10,
+  cores = 4, save_pars = TRUE
+)
+cols <- c("Estimate", "Q2.5", "Q97.5")
+fig_3a_botc <- rounded(brms::fixef(fig_3a_mod)["botc_Intercept", cols], 2)
+fig_3a_alpha <- rounded(brms::fixef(fig_3a_mod)["alpha_Intercept", cols], 2)
+fake_x <- data.frame(
+  x = seq(min(fig_3a_df$x), max(fig_3a_df$x), length.out = 100)
+)
+preds_g <- cbind(fake_x, data.frame(predict(fig_3a_mod, newdata = fake_x)))
+fig_3b_df <- log(fig_3a_df)
+fig_3b_priors <- prior(normal(0, 1), class = "b")
+fig_3b_mod <- brms::brm(
+  y ~ 0 + Intercept + x, data = fig_3b_df, family = gaussian(),
+  prior = fig_3b_priors, chains = 4, iter = 2000, warmup = 1000, thin = 1,
+  seed = 10, cores = 4, save_all_pars = TRUE
+)
+cols <- c("Estimate", "Q2.5", "Q97.5")
+fig_3b_botc <- rounded(brms::fixef(fig_3b_mod)["Intercept", cols], 2)
+fig_3b_alpha <- rounded(brms::fixef(fig_3b_mod)["x", cols], 2)
+fake_x <- data.frame(
+  x = seq(min(fig_3b_df$x), max(fig_3b_df$x), length.out = 100)
+)
+preds_n <- cbind(fake_x, data.frame(predict(fig_3b_mod, newdata = fake_x)))
+fig_3a <- ggplot() +
+  geom_ribbon(
+    data = preds_g, mapping = aes(x = x, ymin = `Q2.5`, ymax = `Q97.5`),
+    fill = "grey90", alpha = 0.8
+  ) +
+  geom_point(data = fig_3a_df, mapping = aes(x, y), alpha = 0.5, size = 2) +
+  geom_line(
+    data = preds_g, mapping = aes(x = x, y = Estimate),
+    colour = "tomato", linetype = 2
+  ) +
+  labs(x = substitute("Body mass, " * italic("M")),
+       y = substitute("Metabolic rate, " * italic("B")),
+       subtitle = substitute(
+         italic("b"["o"]) * "(" * italic("T"["c"]) * ") = 5; " * alpha *
+           " = 0.75"
+       )
+  ) +
+  annotate(
+    geom = "text", x = 0, y = 7500, hjust = 0, vjust = 0.5, size = 3,
+    label = paste0(deparse(
+      substitute(
+        italic("B") == a %.% italic("M")^b,
+        list(a = fig_3a_botc[1], b = fig_3a_alpha[1])
+      )
+    ), collapse = ""), parse = TRUE
+  ) +
+  annotate(
+    geom = "text", x = 0, y = 6800, hjust = 0, vjust = 0.5, size = 3,
+    label = paste0(deparse(
+      substitute(
+        "95% C.I. " * italic("b"["o"]) * "(" * italic("T"["c"]) * "): " *
+          a - b, list(a = fig_3a_botc[2], b = fig_3a_botc[3])
+      )
+    ), collapse = ""), parse = TRUE
+  ) +
+  annotate(
+    geom = "text", x = 0, y = 6100, hjust = 0, vjust = 0.5, size = 3,
+    label = paste0(deparse(
+      substitute(
+        "95% C.I. " * alpha * ": " * a - b,
+        list(a = fig_3a_alpha[2], b = fig_3a_alpha[3])
+      )
+    ), collapse = ""), parse = TRUE
+  ) +
+  theme_classic() +
+  theme(
+     axis.title = element_text(size = .pt * 4, colour = "black"),
+     axis.text = element_text(size = .pt * 3, colour = "black")
+  )
+
+fig_3b <- ggplot() +
+  geom_ribbon(
+    data = preds_n, mapping = aes(x = x, ymin = `Q2.5`, ymax = `Q97.5`),
+    fill = "grey90", alpha = 0.8
+  ) +
+  geom_point(data = fig_3b_df, mapping = aes(x, y), alpha = 0.5, size = 2) +
+  geom_line(
+    data = preds_n, mapping = aes(x = x, y = Estimate),
+    colour = "tomato", linetype = 2
+  ) +
+  labs(x = substitute("ln Body mass, ln" * italic("M")),
+       y = substitute("ln Metabolic rate, ln" * italic("B"))) +
+  annotate(
+    geom = "text", x = 3, y = 8.6, hjust = 0, vjust = 0.5, size = 3,
+    label = paste0(deparse(
+      substitute(
+        "ln" * italic("B") == a + b %.% "ln" * italic("M"),
+        list(a = fig_3b_botc[1], b = fig_3b_alpha[1])
+      )
+    ), collapse = ""), parse = TRUE
+  ) +
+  annotate(
+    geom = "text", x = 3, y = 8.1, hjust = 0, vjust = 0.5, size = 3,
+    label = paste0(deparse(
+      substitute(
+        "95% C.I. " * "ln" * italic("b"["o"]) * "(" * italic("T"["c"]) *
+          "): " * a - b, list(a = fig_3b_botc[2], b = fig_3b_botc[3])
+      )
+    ), collapse = ""), parse = TRUE
+  ) +
+  annotate(
+    geom = "text", x = 3, y = 7.6, hjust = 0, vjust = 0.5, size = 3,
+    label = paste0(deparse(
+      substitute(
+        "95% C.I. " * alpha * ": " * a - b,
+        list(a = fig_3b_alpha[2], b = fig_3b_alpha[3])
+      )
+    ), collapse = ""), parse = TRUE
+  ) +
+  theme_classic() +
+  theme(
+     axis.title = element_text(size = .pt * 4, colour = "black"),
+     axis.text = element_text(size = .pt * 3, colour = "black")
+  )
+
+ggsave("output/figure_3.pdf", fig_3a + fig_3b, width = 7.13, height = 3.23)
